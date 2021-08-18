b37ae5f5bab8076a64c59e93433c14dfe347a407
Furkan Ulutaş <37690550+fulutas@users.noreply.github.com>
2021-08-18 09:10:47 +0300

    Add files via upload

---
+++ Encapsulating Scrolling (Modal)/src/App.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="app">
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+    </p>
+    <div class="mt-2 ml-2">
+      <button type="button" class="btn btn-primary" @click="open = true">
+        Open
+      </button>
+    </div>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et iste cupiditate sapiente blanditiis, aperiam minima molestiae illo accusamus autem maxime ducimus. Deserunt ullam reiciendis aliquam rem eligendi porro hic quam?
+    </p>
+    <app-modal2 :show="open" @hide="open = false" :scrollable="true" />
+  </div>
+</template>
+
+<script>
+import AppModal from '@/components/Modal.vue';
+import AppModal2 from '@/components/ModalWay2.vue';
+
+export default {
+  name: 'App',
+  components: {
+    AppModal,
+    AppModal2
+  },
+  data() {
+    return {
+      open: false,
+    };
+  },
+
+};
+</script>
+
+<style src="bootstrap/dist/css/bootstrap.css">
+</style>
---
+++ Encapsulating Scrolling (Modal)/src/components/ModalWay2.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="modal" :style="{ display: show ? 'block' : 'none' }">
+    <div class="modal-dialog" style="z-index: 2000;">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Awesome Title</h5>
+          <button type="button" class="close" @click="close">
+            <span>&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>Awesome content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="modal-backdrop show"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppModal2",
+  props: {
+    show: {
+      required: true
+    },
+    scrollable: {
+      default: false
+    }
+  },
+  methods: {
+    close() {
+      this.$emit("hide");
+    },
+
+    handler(e) {
+      if (e.code === "Escape" && this.show) {
+        this.close();
+      }
+    }
+  },
+
+  created() {
+    document.addEventListener("keydown", this.handler);
+  },
+
+  unmounted() {
+    document.addEventListener("keydown", this.handler);
+  },
+
+  watch: {
+    show: {
+      immediate: true,
+      handler(newVal) {
+        if (newVal && !this.scrollable) {
+          document.body.style.setProperty("overflow", "hidden");
+        } else {
+          document.body.style.removeProperty("overflow");
+        }
+      }
+    }
+  },
+};
+</script>
+
+<style scoped>
+.modal-dialog {
+  color: #3db43d;
+}
+</style>
